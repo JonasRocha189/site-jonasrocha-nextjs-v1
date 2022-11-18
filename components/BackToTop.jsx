8f91d0331691ad077800b4ff6dec68b9ba6e950b
JonasRocha189 <jonas.rocha189@gmail.com>
2022-11-18 18:37:16 -0300

    adjust in menu and back to top

---
+++ components/BackToTop.jsx
@@ -0,0 +1,37 @@
+import { FaArrowUp } from "react-icons/fa";
+import { useState, useEffect } from "react";
+
+export default function BackToTop(){
+
+  const [showButton, setShowButton] = useState(false);
+
+  function onScroll() {
+    let pixelsFromTop = window.scrollY;
+    let documentHeight = document.body.clientHeight;
+    let windowHeight = window.innerHeight;
+    let difference = documentHeight - windowHeight;
+    let percentage = (100 * pixelsFromTop) / difference;
+    document.getElementById("bar").style.width = `${percentage}%`;
+
+    // Scroll to top button logic
+    window.scrollY > 500 ? setShowButton(true) : setShowButton(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+
+  function scrollToTop () {
+    window.scrollTo(0, 0);
+  };
+
+  return(
+    <>
+      <FaArrowUp
+        className={showButton ? "showButton" : "hidden"}
+        onClick={scrollToTop}
+      />
+    </>
+  )
+}
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Footer from './Footer';
+import BackToTop from './BackToTop';
 import { IoMenu, IoClose, IoMoon, IoSunny, IoLogoReact } from "react-icons/io5";
 
 import {useState} from 'react';
@@ -7,17 +8,38 @@ export default function Layout({children}){
 
   const [darkMode, setDarkMode] = useState(false);
 
-  const handleMenu = (e) => {
+  
 
-    // Toggle mobile menu
+  const showMenu = (e) => {
+
     let list = document.querySelector('ul')
-    list.classList.toggle('top-[75px]')
-    list.classList.toggle('z-10')
+    
+    list.classList.remove("z-[-1]")
+
+    list.classList.add('top-[75px]')
+    list.classList.add('z-10')
 
     // Change icon
-    document.getElementById('menuIcon').classList.toggle('hidden')
-    document.getElementById('closeIcon').classList.toggle('hidden')
+    document.getElementById('menuIcon').classList.add('hidden')
+    document.getElementById('closeIcon').classList.remove('hidden')
+    
   }
+
+  const hideMenu = (e) => {
+
+    let list = document.querySelector('ul')
+    
+    list.classList.add("z-[-1]")
+
+    list.classList.remove('top-[75px]')
+    list.classList.remove('z-10')
+
+    // Change icon
+    document.getElementById('menuIcon').classList.remove('hidden')
+    document.getElementById('closeIcon').classList.add('hidden')
+    
+  }
+
   return(
     <div className={darkMode ? "dark" : ""}>    
       <header className="shadow bg-gray-800 text-gray-200 dark:text-gray-800 dark:bg-gray-400">
@@ -35,12 +57,12 @@ export default function Layout({children}){
             { darkMode ? <IoMoon /> : <IoSunny /> }
           </span>
 
-          <span onClick={handleMenu} className="text-3xl cursor-pointer mx-2 md:hidden block">
-            <IoMenu id="menuIcon" />
-            <IoClose id="closeIcon" className="hidden"/>
+          <span className="text-3xl cursor-pointer mx-2 md:hidden block">
+            <IoMenu id="menuIcon" onClick={showMenu} />
+            <IoClose id="closeIcon" className="hidden" onClick={hideMenu}/>
           </span>
 
-          <ul className="md:flex md:items-center z-[-1] bg-gray-600 md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7">
+          <ul className="md:flex md:items-center z-[-1] bg-gray-800 md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 dark:bg-gray-400">
             <li className="mx-4 my-6 md:my-0" ><a className="text-xl hover:text-cyan-500 duration-500" href="#">Home</a></li>
             <li className="mx-4 my-6 md:my-0" ><a className="text-xl hover:text-cyan-500 duration-500" href="#">Sobre</a></li>
             <li className="mx-4 my-6 md:my-0" ><a className="text-xl hover:text-cyan-500 duration-500" href="#">Portifólio</a></li>
@@ -52,6 +74,12 @@ export default function Layout({children}){
           </ul>
         </nav>
       </header>
+
+      <div className="progress_wrapper">
+        <div className="bg-cyan-600 progress_bar" id="bar"></div>
+      </div>
+
+      <BackToTop />
 
 
       <main>
